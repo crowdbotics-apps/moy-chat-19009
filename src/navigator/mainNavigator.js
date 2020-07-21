a96212a720f01314b9844dbf9a48fe617668704b
Crowdbotics <team@crowdbotics.com>
2020-07-21 03:35:32 +0000

    add package blueprint-react-native-feed-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Feed1282527Navigator from '../features/Feed1282527/navigator';
 import SignUp2782526Navigator from '../features/SignUp2782526/navigator';
 import EmailAuth682524Navigator from '../features/EmailAuth682524/navigator';
 
@@ -15,6 +16,7 @@ import EmailAuth682524Navigator from '../features/EmailAuth682524/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Feed1282527: { screen: Feed1282527Navigator },
 SignUp2782526: { screen: SignUp2782526Navigator },
 EmailAuth682524: { screen: EmailAuth682524Navigator },
 
